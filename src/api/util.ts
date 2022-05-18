f0a55854c7a2e8ce234dc6935747fbc04b9d3d89
정훈박 <fireking5997@gmail.com>
2022-05-18 21:55:01 +0900

    fix:env

--- src/api/util.ts
+++ src/api/util.ts
@@ -1,6 +1,5 @@
 import { baseURL } from './api.constant';
 import { FetchPayload } from './api.type';
-console.log('cdkey', process.env.KEY);
 export const urlMaker = ({ url, params = {} }: FetchPayload) => {
   const urlObject = new URLSearchParams();
   urlObject.append('api_key', process.env.REACT_APP_VERCEL_API_KEY);
